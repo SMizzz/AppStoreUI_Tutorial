cd78e3e99bdec29819d42a33b560548347121f73
SMizzz <shinn.mizzz@gmail.com>
2021-10-15 11:22:04 +0900

    git init

---
+++ Clone_AppStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clone_AppStore
+//
+//  Created by 신미지 on 2021/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
